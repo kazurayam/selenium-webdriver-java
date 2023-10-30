7b407e0d8b4269e9af0d6f7c42bcc9d75b9a1fd8
kazurayam <kazuaki.urayama@gmail.com>
2023-10-30 22:25:07 +0900

    adding HelloWorldFirefoxWithHARTest

---
+++ selenium-webdriver-junit4/src/test/java/io/github/bonigarcia/webdriver/junit4/ch02/helloworld/HelloWorldFirefoxWithHARTest.java
@@ -0,0 +1,83 @@
+package io.github.bonigarcia.webdriver.junit4.ch02.helloworld;
+
+import java.io.IOException;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.client.ClientUtil;
+import net.lightbody.bmp.core.har.Har;
+import net.lightbody.bmp.proxy.CaptureType;
+
+
+public class HelloWorldFirefoxWithHARTest {
+
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    WebDriver driver;
+
+    BrowserMobProxy proxy;
+
+    @Before
+    public void setup() {
+        proxy = new BrowserMobProxyServer();
+        proxy.start();
+        proxy.newHar();
+        proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT,
+                CaptureType.RESPONSE_CONTENT);
+
+        Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+        FirefoxOptions options = new FirefoxOptions();
+        options.setProxy(seleniumProxy);
+        options.setAcceptInsecureCerts(true);
+
+        driver = WebDriverManager.firefoxdriver()
+                .capabilities(options).create();
+        //WebDriverManager.firefoxdriver().useMirror().setup();
+    }
+
+    @After
+    public void teardown() throws IOException {
+        Har har = proxy.getHar();
+        File harFile = new TestHelper.
+        driver.quit();
+    }
+
+    @Test
+    public void test() {
+        // Exercise
+        String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
+        driver.get(sutUrl);
+        String title = driver.getTitle();
+        log.debug("The title of {} is {}", sutUrl, title);
+
+        // Verify
+        assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java");
+    }
+
+}
+
